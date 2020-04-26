d051c5841b92bba95651d404a2c0ee9adce6a2c9
leonel fernandez <leone>
2020-04-26 18:18:45 -0400

    404 and home navigation working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{
-  path: '',
-  loadChildren: () => import('./modules/article/article.module').then(m => m.ArticleModule)
-}];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/article/article.module').then(m => m.ArticleModule)
+  },
+  {
+    path: '404',
+    loadChildren: () => import('./modules/error/error.module').then(m => m.ErrorModule)
+  },
+  {
+    path: '**', redirectTo: '/404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
