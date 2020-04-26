68514e56de9104c92c8917e413cc07915291b4b8
leonel fernandez <leone>
2020-04-26 10:18:14 -0400

    layout done

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { LayoutRoutingModule } from './layout-routing.module';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MainLayoutComponent, TopBarComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    LayoutRoutingModule,
+    MatToolbarModule
+  ],
+  exports: [MainLayoutComponent]
 })
 export class LayoutModule { }
