06557f8332990ee26d2bdcdfe5b07ee202cb39d0
leonel fernandez <leone>
2020-04-26 22:59:25 -0400

    article service unit test done

--- src/app/modules/article/services/article.service.spec.ts
+++ src/app/modules/article/services/article.service.spec.ts
@@ -2,9 +2,10 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { ArticleService } from './article.service';
+import { Article } from './article.model';
+import { environment } from 'src/environments/environment';
 
 describe('ArticleService', () => {
-  let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
 
   let service: ArticleService;
@@ -15,12 +16,66 @@ describe('ArticleService', () => {
       imports: [HttpClientTestingModule]
     });
 
-    httpClient = TestBed.inject(HttpClient);
     httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ArticleService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('Get Articles From Api', () => {
+    const testData: Article[] = [
+      {
+        author: 'auhtor',
+        title: 'title',
+        time: new Date(),
+        url: 'url.com'
+      },
+      {
+        author: 'auhtor',
+        title: 'title',
+        time: new Date(),
+        url: null
+      }
+    ];
+
+    it('GetArticles should call the api once', () => {
+      service.getArticles().subscribe(
+        data => {
+          expect(data).toEqual(testData);
+        }
+      );
+
+      const req = httpTestingController.expectOne(environment.apiUrl);
+
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(testData);
+    });
+
+    it('GetArticlesFiltered should return articles with url', () => {
+      service.getArticles().subscribe(
+        data => {
+          expect(data).toEqual(testData);
+        }
+      );
+
+      const req = httpTestingController.expectOne(environment.apiUrl);
+      req.flush(testData);
+
+      service.getArticlesFiltered().subscribe(
+        data => {
+          var dataHasNull = data.some(d => d.url === null);
+          expect(dataHasNull).toEqual(false);
+        }
+      )
+
+    });
+
+    afterEach(() => {
+      httpTestingController.verify()
+    })
+
   });
 });
